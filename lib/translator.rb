350e7185eddc8d16cdc3674c834610c2caa9d892
Paige Miles <paige@lucas-miles.com>
2020-09-07 23:02:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,9 +12,8 @@ end
 
 def get_japanese_emoticon(yaml, emoticon)
   local_library = load_library(yaml)
-  local_library.each do |key, value|
+  #local_library.each do |key, value|
     #binding.pry
-  end
 end
 
 def get_english_meaning
