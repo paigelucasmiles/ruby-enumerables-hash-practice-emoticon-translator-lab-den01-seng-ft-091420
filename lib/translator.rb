4ce946ec62e3a125f95480cb83d15cb8879b38c0
Paige Miles <paige@lucas-miles.com>
2020-09-07 23:00:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,6 +14,7 @@ def get_japanese_emoticon(yaml, emoticon)
   local_library = load_library(yaml)
   local_library.each do |key, value|
    if value[:english] == emoticon
+     binding.pry
      p value[:japanese]
     end
   end
