e48c6514a517c17aff74a6653ae21027c2ace684
Paige Miles <paige@lucas-miles.com>
2020-09-07 23:02:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,13 +7,14 @@ def load_library(yaml)
   thing.each do |key, values|
     new_library[key] = {:english => values[0], :japanese => values[1]}
   end
-  p new_library
+  new_library
 end
 
 def get_japanese_emoticon(yaml, emoticon)
   local_library = load_library(yaml)
   #local_library.each do |key, value|
     #binding.pry
+    p "hello"
 end
 
 def get_english_meaning
