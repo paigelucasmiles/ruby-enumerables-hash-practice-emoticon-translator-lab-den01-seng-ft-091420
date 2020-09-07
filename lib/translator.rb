815cc723a4da59530363b2f7eb1f1476f6e7f8e8
Paige Miles <paige@lucas-miles.com>
2020-09-07 22:03:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,18 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(yaml)
+  new_library = {}
+  thing = YAML.load_file(yaml)
+  thing.each do |key, values|
+    if !new_library[key]
+      new_library[key] = {:english => values[0], :japanese => values[1]}
+    end
+    p new_library
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(yaml,)
 end
 
 def get_english_meaning
