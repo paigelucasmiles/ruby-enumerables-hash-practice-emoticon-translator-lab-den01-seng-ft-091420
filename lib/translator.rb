71a95447952d00d79d840ac51c236a8ba71a0be2
Paige Miles <paige@lucas-miles.com>
2020-09-07 22:45:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(yaml, english_emoticon)
     if value[:english] == english_emoticon
       puts value[:japanese]
     else 
-      p "Sorry!"
+      p "Sorry, that emoticon was not found"
     end
   end
 end
